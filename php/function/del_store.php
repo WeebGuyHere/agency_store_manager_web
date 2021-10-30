05d115699e1aa951f2929d1cc53af4ff58e7fdd8
2dgirlismywaifu <59259855+2dgirlismywaifu@users.noreply.github.com>
2021-10-30 22:51:35 +0700

    test prevent sql injection

--- php/function/del_store.php
+++ php/function/del_store.php
@@ -3,12 +3,11 @@ session_start();
 require_once('../../lib/connection.php');
 
 $id = $_GET['id']; 
-$qry = mysqli_query($conn,"SELECT * FROM cacdaily where MaDaiLy='$id'"); 
-$data = mysqli_fetch_array($qry); 
 if (isset ($_SESSION['username']) )
 {
-        $del_sql = mysqli_query($conn,"DELETE FROM cacdaily WHERE MaDaiLy = '$id'"); 
-        mysqli_close($conn); 
+        $del_sql = $conn->prepare("DELETE FROM cacdaily WHERE MaDaiLy = ?"); 
+        $del_sql->bind_param("s", $id);
+        $del_sql->execute();
         header("location:../ThemDaiLy.php"); 
         exit;	 
 }
