327a3cba8761abcc633a9a09eafa7c3f7bcdeb9b
2dgirlismywaifu <59259855+2dgirlismywaifu@users.noreply.github.com>
2021-10-31 13:09:03 +0700

    All file name change to english. Please follow new name file and change it right to English

--- index.php
+++ index.php
@@ -39,12 +39,12 @@
                     <li><a href="./index.php"><span><ion-icon name="home-outline"></ion-icon></span> Home</a></li>
                     <li><a href="#">Tổ chức</a>
                       <ul class ="menu-dropdown">
-                        <li><a href="./php/ThemdaiLy.php">Quản lý các đại lý</a></li>
-                        <li><a href="./php/ThemMatHang.php">Quản lý mặt hàng</a></li>
+                        <li><a href="./php/AgencyStore.php">Quản lý các đại lý</a></li>
+                        <li><a href="./php/addgoods.php">Quản lý mặt hàng</a></li>
                         <li><a href="#">Xuất phiếu</a>
                         <ul class ="menu-dropdown1">
-                        <li><a href="./php/PhieuThuTien.php">Phiếu thu tiền</a></li>
-                        <li><a href="./php/Phieuxuathang.php">Phiếu xuất hàng</a></li>
+                        <li><a href="./php/Bill.php">Phiếu thu tiền</a></li>
+                        <li><a href="./php/DeliveryNote.php">Phiếu xuất hàng</a></li>
                         
                       </li>
                       </ul>
@@ -54,13 +54,13 @@
              
                     <li><a href="#">Báo cáo</a>
                     <ul class ="menu-dropdown">
-                      <li><a href="./php/DoanhSo.php">Doanh số</a></li>
-                    <li><a href="./php/CongNo.php">Công nợ tháng</a></li>
+                      <li><a href="./php/SalesReport.php">Doanh số</a></li>
+                    <li><a href="./php/DebtReport.php">Công nợ tháng</a></li>
                     </li></ul>
                     <li><a href="#">Khách hàng</a>
                       <ul class ="menu-dropdown">
-                    <li><a href="./php/PhanHoi.php">Phản hồi</a></li>
-                    <li><a href="./php/YeuCau.php">Yêu cầu Khác hàng</a></li></li></ul>
+                    <li><a href="./php/Feedback.php">Phản hồi</a></li>
+                    <li><a href="./php/Request.php">Yêu cầu Khác hàng</a></li></li></ul>
                     <li><a href="#">Contact</a>
                     <ul class="menu-dropdown">
                       <li><a href="#">Hotline: 0948232811</a></li>
