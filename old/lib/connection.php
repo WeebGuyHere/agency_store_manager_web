c06daed93ceec3eadacd454328dbf4a76aed2116
2dgirlismywaifu <59259855+2dgirlismywaifu@users.noreply.github.com>
2021-11-07 01:17:39 +0700

    good bye old work, all of its now useless

--- old/lib/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = ""; //change password match with your own mysql. If mysql not set password, change to ''
-$database = "quanlydaily";
-
-$conn = new mysqli($servername, $username, $password, $database) or die("Can't Connect to database: ");
-mysqli_query($conn, "SET NAME 'UTF-8");
-?>
